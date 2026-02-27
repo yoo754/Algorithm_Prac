e9821bb27394542b647ea218bfb28c78f47ba2ce
정혜진 <51073787+yoo754@users.noreply.github.com>
2026-02-27 14:22:39 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.16 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,47 +1,37 @@
 import java.util.*;
-
 class Solution {
-    public int solution(int[][] maps) {        
-        int N = maps.length;
-        int M = maps[0].length;
-        
-        //bfs
-        
+    public int solution(int[][] maps) {
         int[] dr = {-1,1,0,0};
         int[] dc = {0,0,-1,1};
         
-        // Set<String> visited = new HashSet<>(); // visited set
+        int N = maps.length;
+        int M = maps[0].length;
+        
         boolean[][] visited = new boolean[N][M];
-        Queue<int[]> queue = new LinkedList<>(); // queue
+        Deque<int[]> dq = new ArrayDeque<>();
+        int[] start = {0,0,1};
         
-        queue.add(new int[]{0,0,1});
-        visited[0][0] = true;
+        dq.add(start);
+        visited[0][0]= true;
         
-        while(!queue.isEmpty()) {
-            int[] now = queue.poll();
+        while(!dq.isEmpty()) {
+            int[] now = dq.poll();
             int r = now[0];
             int c = now[1];
             int dist = now[2];
             
-            if(r == (N-1) && c == (M-1)) {
+            if(r == N-1 && c == M-1) {
                 return dist;
             }
-            
-            
             for(int d=0; d<4; d++) {
                 int nr = dr[d] + r;
                 int nc = dc[d] + c;
                 
-                if(0<=nr && nr<N &&
-                  0<=nc && nc<M &&
-                  maps[nr][nc] == 1 &&
-                  !visited[nr][nc]) {
-                    
+                if(0<=nr && nr<N && 0<=nc && nc<M && maps[nr][nc] == 1 && !visited[nr][nc]) {
                     visited[nr][nc] = true;
-                    queue.add(new int[]{nr, nc, dist+1});
-
+                    int[] next = {nr,nc,dist+1};
+                    dq.add(next);
                 }
-                
             }
         }
         
